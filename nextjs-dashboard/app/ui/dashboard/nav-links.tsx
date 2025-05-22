88f03998f33c91322f3f0b0050e1b6009e9803a1
rakellbandeira <rakelldesigner@gmail.com>
2025-05-21 23:04:53 -0400

    fix route 1

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -13,11 +13,8 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Overview', href: '/dashboard/overview', icon: HomeIcon }, // Add this if needed
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
