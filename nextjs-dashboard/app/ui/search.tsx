d65c48c8f23944151d650084490e930b0845762f
rakellbandeira <rakelldesigner@gmail.com>
2025-05-25 15:39:34 -0400

    run the code after stopping type 3s

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,7 +2,8 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import { useDebouncedCallback } from 'use-debounce';
+ 
  
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -12,17 +13,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearch(term: string) {
     console.log(`Searching... ${term}`);
-    
-    const params = new URLSearchParams(searchParams);
 
-
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-
+    const handleSearch = useDebouncedCallback((term) => {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+          params.set('query', term);
+        } else {
+          params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }, 300);
     // That was "Update the URL with the search params"
 
     console.log(term);
